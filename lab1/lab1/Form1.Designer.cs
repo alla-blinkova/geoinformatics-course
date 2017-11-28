5a90bfc479c29fa0b11ff3cb6a491d269445d215
alla-blinkova <blinkaa@mail.ru>
2017-11-28 15:24:14 +0300

    add lab1

---
+++ lab1/lab1/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.pictureBox1.Location = new System.Drawing.Point(50, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.pictureBox2.Location = new System.Drawing.Point(306, 32);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.pictureBox3.Location = new System.Drawing.Point(50, 288);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.Highlight;
+            this.pictureBox4.Location = new System.Drawing.Point(306, 288);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Location = new System.Drawing.Point(699, 113);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(75, 23);
+            this.buttonUp.TabIndex = 4;
+            this.buttonUp.Text = "up";
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonUp_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Location = new System.Drawing.Point(631, 152);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(75, 23);
+            this.buttonLeft.TabIndex = 5;
+            this.buttonLeft.Text = "left";
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonLeft_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Location = new System.Drawing.Point(764, 152);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(75, 23);
+            this.buttonRight.TabIndex = 6;
+            this.buttonRight.Text = "right";
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonRight_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Location = new System.Drawing.Point(699, 191);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(75, 23);
+            this.buttonDown.TabIndex = 7;
+            this.buttonDown.Text = "down";
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonDown_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(699, 265);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(699, 305);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(851, 622);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Lab1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ lab1/lab1/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+        private int startLevel = 5;
+        private int startX = 8;
+        private int startY = 4;
+        private String startPath = "D:\\geoscan_labs\\map\\z";
+
+        private List<PictureBox> pBoxes;
+        private int currentLevel;
+        private int currentX;
+        private int currentY;
+
+        public String ConstructPath(int level, int x, int y)
+        {
+            String path = startPath + level.ToString() + "\\0\\x" + x.ToString() + "\\0\\y" + y.ToString() + ".png";
+            return path;
+        }
+
+        public void LoadTiles(int level, int startX, int startY)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                pBoxes[i].Image = null;
+            }
+
+            LoadFile(ConstructPath(level, startX, startY), 0);
+            LoadFile(ConstructPath(level, startX + 1, startY), 1);
+            LoadFile(ConstructPath(level, startX, startY + 1), 2);
+            LoadFile(ConstructPath(level, startX + 1, startY + 1), 3);
+        }
+
+        public void LoadFile(String path, int index)
+        {
+            Bitmap tile;
+            if (File.Exists(path))
+            {
+                tile = new Bitmap(path);
+                pBoxes[index].Image = (Image)tile;
+            }
+
+        }
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pBoxes = new List<PictureBox>();
+            pBoxes.Add(pictureBox1);
+            pBoxes.Add(pictureBox2);
+            pBoxes.Add(pictureBox3);
+            pBoxes.Add(pictureBox4);
+
+            LoadTiles(startLevel, startX, startY);
+
+            currentX = startX;
+            currentY = startY;
+            currentLevel = startLevel;
+            button2.Enabled = false;
+
+        }
+
+        private void buttonDown_Click(object sender, EventArgs e)
+        {
+            currentY++;
+            LoadTiles(currentLevel, currentX, currentY);
+        }
+
+        private void buttonUp_Click(object sender, EventArgs e)
+        {
+            currentY--;
+            LoadTiles(currentLevel, currentX, currentY);
+        }
+
+        private void buttonRight_Click(object sender, EventArgs e)
+        {
+            currentX++;
+            LoadTiles(currentLevel, currentX, currentY);
+        }
+
+        private void buttonLeft_Click(object sender, EventArgs e)
+        {
+            currentX--;
+            LoadTiles(currentLevel, currentX, currentY);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            currentLevel++;
+            currentX = (currentX + 1) * 2 - 1;
+            currentY = (currentY + 1) * 2 - 1;
+
+            LoadTiles(currentLevel, currentX, currentY);
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            currentLevel--;
+            currentX /= 2;
+            currentY /= 2;
+
+            LoadTiles(currentLevel, currentX, currentY);
+
+            if (currentLevel == startLevel)
+            {
+                button2.Enabled = false;
+            }
+        }
+    }
+}
